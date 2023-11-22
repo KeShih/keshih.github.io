77c764d476f28196c340404c616f8e67af1d4f0c
KeShih <self.ke.shi@gmail.com>
2023-11-22 20:56:35 +0800

    update index

--- push.sh
+++ push.sh
@@ -15,7 +15,7 @@ export NODE_PATH=$(npm root --quiet -g)
 # Build new files
 # stack build --ghc-options=-O2
 # stack exec chaosite clean
-stack exec chaosite build
+c
 
 # Build index
 python3 pub.py > _site/index.html
