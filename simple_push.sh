b6ebaa4617ddd048f58c2d291cf99baae34005de
KeShih <self.ke.shi@gmail.com>
2024-08-10 05:11:43 +0800

    update

--- simple_push.sh
+++ simple_push.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # make sure in develop branch
-git checkout master
+git checkout develop
 
 # push dev to remote
 git add . && git commit -m "update" && git push origin develop:develop
